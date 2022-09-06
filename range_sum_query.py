72b1b23a644769709bb2c104db836155a927eaff
Andreas Hiropedi <s2015345@ed.ac.uk>
2022-09-06 14:57:57 +0300

    Create range_sum_query.py

---
+++ range_sum_query.py
@@ -0,0 +1,18 @@
+class NumArray(object):
+
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        """
+        self.nums = nums
+
+    def sumRange(self, left, right):
+        """
+        :type left: int
+        :type right: int
+        :rtype: int
+        """
+        total = 0
+        for i in range(left, right+1):
+            total = total + self.nums[i]
+        return total
