ad0770469876bdc4b59ed571d8a2e4e6442a562c
Andreas Hiropedi <s2015345@ed.ac.uk>
2022-09-06 19:05:00 +0300

    Create merge_intervals.py

---
+++ merge_intervals.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        if len(intervals) == 1:
+            return intervals
+        
+        # Sort the array on the basis of start values of intervals.
+        intervals.sort()
+        stack = []
+        # insert first interval into stack
+        stack.append(intervals[0])
+        for i in intervals[1:]:
+            # Check for overlapping interval,
+            # if interval overlap
+            if stack[-1][0] <= i[0] <= stack[-1][-1]:
+                stack[-1][-1] = max(stack[-1][-1], i[-1])
+            else:
+                stack.append(i)
+        return stack
